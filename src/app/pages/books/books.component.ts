432aa6a1d8732a46c9136fe042aae611b47e5ba8
Lucas Sansberro <lucas_sansberro@outlook.com>
2024-01-11 08:38:07 -0300

    handlePageEvent in paginator

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -11,30 +11,24 @@ import { DataService } from 'src/app/services/data.service';
 export class BooksComponent implements OnInit {
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator | undefined;
   books: Book[] = [];
+  renderingBooks: Book[] = []
   constructor(private dataService: DataService) {}
   ngOnInit(): void {
-    this.books = this.dataService.books;
-    this.books.push(this.dataService.book1);
-    this.books.push(this.dataService.book2);
-    this.books.push(this.dataService.book3);
-    this.books.push(this.dataService.book1);
-    this.books.push(this.dataService.book2);
-    this.books.push(this.dataService.book3);
-    this.books.push(this.dataService.book1);
-    this.books.push(this.dataService.book2);
-    this.books.push(this.dataService.book3);
-    this.books.push(this.dataService.book1);
-    this.books.push(this.dataService.book2);
-    this.books.push(this.dataService.book3);
-    this.books.push(this.dataService.book1);
-    this.books.push(this.dataService.book2);
-    this.books.push(this.dataService.book3);
-    this.books.push(this.dataService.book1);
-    this.books.push(this.dataService.book2);
-    this.books.push(this.dataService.book3);
     this.paginator!._intl.itemsPerPageLabel="Libros por página";
+    this.books = this.dataService.books;
+    for (let i = 0; i < 30; i++) {
+      this.books.push(this.dataService.book1);
+      this.books.push(this.dataService.book2);
+      this.books.push(this.dataService.book3);
+    }
+    this.renderingBooks = this.books.slice(0,18)
+
   }
   handlePageEvent(event: any) {
-    console.log('hola');
+    this.renderingBooks = this.books.slice(0,event.pageSize)
+  }
+  pageIndexEvent(number:any){
+    console.log("Hola")
+    return 2
   }
 }
