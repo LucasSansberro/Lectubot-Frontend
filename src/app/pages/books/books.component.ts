47e70887e3204c55b4d23b131ec60e032276b0df
Lucas Sansberro <lucas_sansberro@outlook.com>
2024-01-12 08:08:45 -0300

    basic paginator finished

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -3,32 +3,45 @@ import { MatPaginator } from '@angular/material/paginator';
 import { Book } from 'src/app/models/Entities/Book';
 import { DataService } from 'src/app/services/data.service';
 
+interface PageEvent {
+  pageIndex: number;
+  pageSize: number;
+}
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css'],
 })
 export class BooksComponent implements OnInit {
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator | undefined;
+  @ViewChild(MatPaginator, { static: true }) paginator:
+    | MatPaginator
+    | undefined;
   books: Book[] = [];
-  renderingBooks: Book[] = []
+  renderingBooks: Book[] = [];
+  pageIndex: number = 0;
+  pageSize: number = 18;
+
   constructor(private dataService: DataService) {}
+
   ngOnInit(): void {
-    this.paginator!._intl.itemsPerPageLabel="Libros por página";
+    this.paginator!._intl.itemsPerPageLabel = 'Libros por página';
     this.books = this.dataService.books;
     for (let i = 0; i < 30; i++) {
       this.books.push(this.dataService.book1);
       this.books.push(this.dataService.book2);
       this.books.push(this.dataService.book3);
     }
-    this.renderingBooks = this.books.slice(0,18)
+    this.renderingBooks = this.renderBooks(this.pageIndex, this.pageSize);
+  }
 
+  handlePageEvent(event: PageEvent) {
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.renderingBooks = this.renderBooks(this.pageIndex, this.pageSize);
   }
-  handlePageEvent(event: any) {
-    this.renderingBooks = this.books.slice(0,event.pageSize)
-  }
-  pageIndexEvent(number:any){
-    console.log("Hola")
-    return 2
+
+  renderBooks(pageIndex: number, pageSize: number): Book[] {
+    return this.books.slice(pageIndex * pageSize, (pageIndex + 1) * pageSize);
   }
 }
