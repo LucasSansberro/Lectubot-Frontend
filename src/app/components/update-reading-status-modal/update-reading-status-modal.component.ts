422e3f46e25476576a532191b752906f906ce1ec
Lucas Sansberro <lucas_sansberro@outlook.com>
2024-01-26 08:33:52 -0300

    sweet alert added. Close modal function added

--- src/app/components/update-reading-status-modal/update-reading-status-modal.component.ts
+++ src/app/components/update-reading-status-modal/update-reading-status-modal.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-update-reading-status-modal',
   templateUrl: './update-reading-status-modal.component.html',
-  styleUrl: './update-reading-status-modal.component.css'
+  styleUrl: './update-reading-status-modal.component.css',
 })
 export class UpdateReadingStatusModalComponent {
+  @ViewChild('closeButton') closeButton!: ElementRef;
 
+  showAlert(title: string, text: string) {
+    Swal.fire({
+      title,
+      text,
+      icon: 'success',
+    });
+    this.closeModal()
+  }
+  closeModal() {
+    (this.closeButton.nativeElement as HTMLElement).click();
+  }
 }
