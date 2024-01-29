f63d172160eb783d4934e6b554872b11e5b95cfa
Lucas Sansberro <lucas_sansberro@outlook.com>
2024-01-29 09:00:57 -0300

    styles added to mat-button-group

--- src/app/components/update-reading-status-modal/update-reading-status-modal.component.ts
+++ src/app/components/update-reading-status-modal/update-reading-status-modal.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
 import Swal from 'sweetalert2';
@@ -6,17 +13,24 @@ import Swal from 'sweetalert2';
   selector: 'app-update-reading-status-modal',
   templateUrl: './update-reading-status-modal.component.html',
   styleUrl: './update-reading-status-modal.component.css',
+  animations: [
+    trigger('expandTextarea', [
+      state('yes', style({ height: '150px' })),
+      state('no', style({ height: '0' })),
+      transition('no <=> yes', animate('5s ease')),
+    ]),
+  ],
 })
 export class UpdateReadingStatusModalComponent {
   @ViewChild('closeButton') closeButton!: ElementRef;
-
+  selectedValue: string = 'no';
   showAlert(title: string, text: string) {
     Swal.fire({
       title,
       text,
       icon: 'success',
     });
-    this.closeModal()
+    this.closeModal();
   }
   closeModal() {
     (this.closeButton.nativeElement as HTMLElement).click();
