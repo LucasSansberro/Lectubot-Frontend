f260aad9cf5273c89e99d14aef7482f3295e1aa0
Lucas Sansberro <lucas_sansberro@outlook.com>
2024-02-17 08:52:08 -0300

    added filter component to add-book-modal

--- src/app/components/add-book-modal/add-book-modal.component.ts
+++ src/app/components/add-book-modal/add-book-modal.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Genre } from 'src/app/models/Enums/Genre';
 
 @Component({
   selector: 'app-add-book-modal',
   templateUrl: './add-book-modal.component.html',
-  styleUrl: './add-book-modal.component.css'
+  styleUrl: './add-book-modal.component.css',
 })
 export class AddBookModalComponent {
-
+  @Input() genres: string[] = [];
+  constructor() {
+    this.genres = Object.values(Genre);
+  }
 }
